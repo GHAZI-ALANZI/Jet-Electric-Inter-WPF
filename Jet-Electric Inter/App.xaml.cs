b956dfa4e9fa52e744437e9c8d12d50018c579d6
GHAZI ALANZI <105205339+GHAZI-ALANZI@users.noreply.github.com>
2023-12-17 10:34:05 +0100

    init app

---
+++ Jet-Electric Inter/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Jet_Electric_Inter
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
