4322e3e0011b0a01a29708c92659385fe903f824
GHAZI ALANZI <105205339+GHAZI-ALANZI@users.noreply.github.com>
2023-12-18 14:11:04 +0100

    add logic to hash password and validation login

---
+++ Jet-Electric Inter/Logic/LoggedInUser.cs
@@ -0,0 +1,32 @@
+﻿
+
+using Jet_Electric_DataAccessLayer.Models;
+
+namespace Jet_Electric_Inter.Logic
+{
+    public class LoggedInUser
+    {
+        public Login Info;
+        public void RemoveData()
+        {
+            Info = null;
+        }
+        private static LoggedInUser instance;
+
+        private LoggedInUser() { }
+
+        public static LoggedInUser Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new LoggedInUser();
+                }
+                return instance;
+            }
+        }
+
+
+    }
+}
---
+++ Jet-Electric Inter/Logic/ValidationRules.cs
@@ -0,0 +1,69 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+
+namespace Jet_Electric_Inter.Logic
+{
+    public class NotEmptyValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            return string.IsNullOrWhiteSpace((value ?? "").ToString())
+                ? new ValidationResult(false, "Field is required.")
+                : ValidationResult.ValidResult;
+        }
+    }
+
+    public class FutureDateValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (!DateTime.TryParse((value ?? "").ToString(),
+                CultureInfo.CurrentCulture,
+                DateTimeStyles.AssumeLocal | DateTimeStyles.AllowWhiteSpaces,
+                out var time))
+            {
+                return new ValidationResult(false, "Invalid date");
+            }
+
+            return time.Date <= DateTime.Now.Date
+                ? new ValidationResult(false, "Future date required")
+                : ValidationResult.ValidResult;
+        }
+    }
+
+    public class PresentFutureDateValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (!DateTime.TryParse((value ?? "").ToString(),
+                CultureInfo.CurrentCulture,
+                DateTimeStyles.AssumeLocal | DateTimeStyles.AllowWhiteSpaces,
+                out var time))
+            {
+                return new ValidationResult(false, "Invalid date");
+            }
+
+            return time.Date <= DateTime.Now.Date.AddDays(-1)
+                ? new ValidationResult(false, "Present or Future date required")
+                : ValidationResult.ValidResult;
+        }
+    }
+
+    public class PastDateValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (!DateTime.TryParse((value ?? "").ToString(),
+                CultureInfo.CurrentCulture,
+                DateTimeStyles.AssumeLocal | DateTimeStyles.AllowWhiteSpaces,
+                out var time))
+            {
+                return new ValidationResult(false, "Invalid date");
+            }
+
+            return time.Date >= DateTime.Now.Date.AddDays(1)
+                ? new ValidationResult(false, "Present or Past date required")
+                : ValidationResult.ValidResult;
+        }
+    }
+}
