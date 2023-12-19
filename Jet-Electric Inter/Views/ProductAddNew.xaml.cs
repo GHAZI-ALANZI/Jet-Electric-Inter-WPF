75463d6ea2ba829172ac083c2ecd6361191b8f5b
GHAZI ALANZI <105205339+GHAZI-ALANZI@users.noreply.github.com>
2023-12-19 22:06:01 +0100

    Product views add and edit done

--- Jet-Electric Inter/Views/ProductAddNew.xaml.cs
+++ Jet-Electric Inter/Views/ProductAddNew.xaml.cs
@@ -1,17 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Jet_Electric_DataAccessLayer.Factories;
+using Jet_Electric_DataAccessLayer.Models;
+using MaterialDesignThemes.Wpf;
+using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using ViewModels;
 
 namespace Jet_Electric_Inter.Views
 {
@@ -20,9 +13,257 @@ namespace Jet_Electric_Inter.Views
     /// </summary>
     public partial class ProductAddNew : UserControl
     {
+        ProductViewModel _vm;
+        BackgroundWorker worker;
         public ProductAddNew()
         {
             InitializeComponent();
+            _vm = new ProductViewModel();
+            this.DataContext = _vm;
         }
+
+        //Executing after loading window, refer to XAML
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            worker = new BackgroundWorker();
+            worker.DoWork += Worker_DoWork;
+            worker.RunWorkerAsync();
+        }
+
+        void Worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            List<Make> mklist = new MakeFactory().SelectAll();
+            List<Jet_Electric_DataAccessLayer.Models.Type> paneltypelist = new TypeFactory().SelectAll();
+            List<SizeType> szlist = new SizeTypeFactory().SelectAll();
+            List<PanelShellGradeProtection> shellgradelist = new PanelShellGradeProtectionFactory().SelectAll();
+            List<Certification> certlist = new CertificationFactory().SelectAll();
+            List<PaType> parttypelist = new PaTypeFactory().SelectAll();
+            List<Location> locationlist = new LocationFactory().SelectAll();
+
+            this.Dispatcher.Invoke(() =>
+            {
+                // Generic
+                MakeComboBox.ItemsSource = mklist;
+                MakeComboBox.DisplayMemberPath = "Name";
+                MakeComboBox.SelectedValuePath = "ID";
+
+                //Panel Type
+                PanelTypeComboBox.ItemsSource = paneltypelist;
+                PanelTypeComboBox.DisplayMemberPath = "Description";
+                PanelTypeComboBox.SelectedValuePath = "ID";
+
+                SizeComboBox.ItemsSource = szlist;
+                SizeComboBox.DisplayMemberPath = "Description";
+                SizeComboBox.SelectedValuePath = "ID";
+
+                PanelIPNumberComboBox.ItemsSource = shellgradelist;
+                PanelIPNumberComboBox.DisplayMemberPath = "IPNum";
+                PanelIPNumberComboBox.SelectedValuePath = "ID";
+
+                CertComboBox.ItemsSource = certlist;
+                CertComboBox.DisplayMemberPath = "Name";
+                CertComboBox.SelectedValuePath = "ID";
+
+                //Part Type
+                PartTypeComboBox.ItemsSource = parttypelist;
+                PartTypeComboBox.DisplayMemberPath = "Name";
+                PartTypeComboBox.SelectedValuePath = "ID";
+
+                //Location
+                LocationComboBox.ItemsSource = locationlist;
+                LocationComboBox.DisplayMemberPath = "Name";
+                LocationComboBox.SelectedValuePath = "ID";
+            });
+        }
+
+        private void P_Panel_Checked(object sender, RoutedEventArgs e)
+        {
+            //Panel
+            TxtBxStackPanel4.Visibility = Visibility.Collapsed;
+
+            //Part
+            TxtBxStackPanel0.Visibility = Visibility.Visible;
+            TxtBxStackPanel1.Visibility = Visibility.Visible;
+            TxtBxStackPanel2.Visibility = Visibility.Visible;
+            TxtBxStackPanel3.Visibility = Visibility.Visible;
+        }
+
+        private void P_Product_Checked(object sender, RoutedEventArgs e)
+        {
+            //Panel
+            TxtBxStackPanel4.Visibility = Visibility.Visible;
+
+            //Part
+            TxtBxStackPanel0.Visibility = Visibility.Collapsed;
+            TxtBxStackPanel1.Visibility = Visibility.Collapsed;
+            TxtBxStackPanel2.Visibility = Visibility.Collapsed;
+            TxtBxStackPanel3.Visibility = Visibility.Collapsed;
+        }
+
+        private void ForceValidation()
+        {
+            NameTextBox.GetBindingExpression(TextBox.TextProperty).UpdateSource();
+            SerialNoTextBox.GetBindingExpression(TextBox.TextProperty).UpdateSource();
+            StockLevelTextBox.GetBindingExpression(TextBox.TextProperty).UpdateSource();
+        }
+
+
+        private void InsertButton_Click(object sender, RoutedEventArgs e)
+        {
+            #region validation 
+            ForceValidation();
+            if (Validation.GetHasError(NameTextBox) || Validation.GetHasError(SerialNoTextBox) || Validation.GetHasError(StockLevelTextBox))
+            {
+                var sMessageDialog = new MessageDialog
+                {
+                    Message = { Text =
+                    "ERROR: Fill required fields!" }
+                };
+
+                DialogHost.Show(sMessageDialog, "RootDialog");
+                return;
+            }
+            else if (!(bool)P_Panel.IsChecked && !(bool)P_Part.IsChecked)
+            {
+                var sMessageDialog = new MessageDialog
+                {
+                    Message = { Text =
+                    "ERROR: Select either part or panel!" }
+                };
+
+                DialogHost.Show(sMessageDialog, "RootDialog");
+                return;
+            }
+            else if ((bool)P_Panel.IsChecked)
+            {
+                if (MakeComboBox.SelectedValue == null ||
+                    LocationComboBox.SelectedValue == null ||
+                    PanelTypeComboBox.SelectedValue == null ||
+                    SizeComboBox.SelectedValue == null ||
+                    PanelIPNumberComboBox.SelectedValue == null ||
+                    CertComboBox.SelectedValue == null
+                    )
+                {
+                    var sMessageDialog = new MessageDialog
+                    {
+                        Message = { Text =
+                    "ERROR: Select all fields!" }
+                    };
+
+                    DialogHost.Show(sMessageDialog, "RootDialog");
+                    return;
+                }
+            }
+            else if ((bool)P_Part.IsChecked)
+            {
+                if (MakeComboBox.SelectedValue == null ||
+                    LocationComboBox.SelectedValue == null ||
+                    PartTypeComboBox.SelectedValue == null
+                    )
+                {
+                    var sMessageDialog = new MessageDialog
+                    {
+                        Message = { Text =
+                    "ERROR: Select all fields!" }
+                    };
+
+                    DialogHost.Show(sMessageDialog, "RootDialog");
+                    return;
+                }
+            }
+
+            #endregion
+
+            if ((bool)P_Panel.IsChecked)
+            {
+                Jet_Electric_DataAccessLayer.Models.Panel panel = new Jet_Electric_DataAccessLayer.Models.Panel()
+                {
+                    Name = NameTextBox.Text,
+                    SerialNo = SerialNoTextBox.Text,
+                    MakeID = int.Parse(MakeComboBox.SelectedValue.ToString()),
+                    TypeID = int.Parse(PanelTypeComboBox.SelectedValue.ToString()),
+                    SizeTypeID = int.Parse(SizeComboBox.SelectedValue.ToString()),
+                    PanelShellGradeProtectionIPNumber = int.Parse(PanelIPNumberComboBox.SelectedValue.ToString()),
+                    CertificationID = int.Parse(CertComboBox.SelectedValue.ToString())
+                };
+
+                Product incomingNewProduct = new Jet_Electric_DataAccessLayer.Models.Panel();
+                incomingNewProduct = panel;
+                AddProd(incomingNewProduct);
+                AddProdToLocation(incomingNewProduct,
+                    int.Parse(StockLevelTextBox.Text),
+                    int.Parse(LocationComboBox.SelectedValue.ToString()));
+            }
+
+            if ((bool)P_Part.IsChecked)
+            {
+                Part part = new Jet_Electric_DataAccessLayer.Models.Part()
+                {
+                    Name = NameTextBox.Text,
+                    SerialNo = SerialNoTextBox.Text,
+                    MakeID = int.Parse(MakeComboBox.SelectedValue.ToString()),
+
+                    PaTypeID = int.Parse(PartTypeComboBox.SelectedValue.ToString())
+                };
+
+                Product incomingNewProduct = new Part();
+                incomingNewProduct = part;
+                AddProd(incomingNewProduct);
+                AddProdToLocation(incomingNewProduct,
+                    int.Parse(StockLevelTextBox.Text),
+                    int.Parse(LocationComboBox.SelectedValue.ToString()));
+            }
+
+            ClearButton_Click(null, null);
+        }
+
+        private void AddProdToLocation(Product prod, int stockLevel, int locID)
+        {
+            var inven = new Inventory()
+            {
+                ID = prod.ID,
+                StockLevel = stockLevel,
+                LocationID = locID,
+            };
+            new InventoryFactory().InsertInventory(inven);
+        }
+
+        private void AddProd(Product incomingNewProduct)
+        {
+            if (new ProductFactory().InsertProduct(incomingNewProduct))
+            {
+                var sMessageDialog = new MessageDialog
+                {
+                    Message = { Text = "Added succesfully!" }
+                };
+                DialogHost.Show(sMessageDialog, "RootDialog");
+                return;
+            }
+            else
+            {
+                var sMessageDialog = new MessageDialog
+                {
+                    Message = { Text = "Couldn't Insert!" }
+                };
+                DialogHost.Show(sMessageDialog, "RootDialog");
+                return;
+            }
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            NameTextBox.Clear();
+            StockLevelTextBox.Clear();
+            SerialNoTextBox.Clear();
+
+            MakeComboBox.SelectedItem = null;
+            CertComboBox.SelectedItem = null;
+            PanelIPNumberComboBox.SelectedItem = null;
+            SizeComboBox.SelectedItem = null;
+            PanelTypeComboBox.SelectedItem = null;
+            PartTypeComboBox.SelectedItem = null;
+            LocationComboBox.SelectedItem = null;
+        }
+
     }
 }
--- Jet-Electric Inter/Views/ProductEdit.xaml.cs
+++ Jet-Electric Inter/Views/ProductEdit.xaml.cs
@@ -1,17 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Jet_Electric_DataAccessLayer.Factories;
+using Jet_Electric_DataAccessLayer.Models;
+using MaterialDesignThemes.Wpf;
+using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using ViewModels;
 
 namespace Jet_Electric_Inter.Views
 {
@@ -20,9 +13,221 @@ namespace Jet_Electric_Inter.Views
     /// </summary>
     public partial class ProductEdit : UserControl
     {
+        ProductViewModel _vm;
+        //Adding backgroud worker
+        BackgroundWorker worker;
+        List<Product> prodlist;
         public ProductEdit()
         {
             InitializeComponent();
+            _vm = new ProductViewModel();
+            this.DataContext = _vm;
+        }
+
+
+        //Executing after loading window, refer to XAML
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            worker = new BackgroundWorker();
+            worker.DoWork += Worker_DoWork;
+            worker.RunWorkerAsync();
+        }
+
+        void Worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            prodlist = new ProductFactory().SelectAll();
+            List<Make> mklist = new MakeFactory().SelectAll();
+            List<Location> locList = new LocationFactory().SelectAll();
+            this.Dispatcher.Invoke(() =>
+            {
+                MakeComboBox.ItemsSource = mklist;
+                MakeComboBox.DisplayMemberPath = "Name";
+                MakeComboBox.SelectedValuePath = "ID";
+
+                SelectProductComboBox.ItemsSource = prodlist;
+                SelectProductComboBox.DisplayMemberPath = "Name";
+                SelectProductComboBox.SelectedValuePath = "ID";
+
+                LocationComboBox.ItemsSource = locList;
+                LocationComboBox.DisplayMemberPath = "Name";
+                LocationComboBox.SelectedValuePath = "ID";
+            });
+        }
+
+        private void ForceValidation()
+        {
+            this.SerialNoTextBox.GetBindingExpression(TextBox.TextProperty).UpdateSource();
+            this.StockLevelTextBox.GetBindingExpression(TextBox.TextProperty).UpdateSource();
+            this.NameTextBox.GetBindingExpression(TextBox.TextProperty).UpdateSource();
+            this.MakeComboBox.GetBindingExpression(ComboBox.SelectedValueProperty).UpdateSource();
+            this.LocationComboBox.GetBindingExpression(ComboBox.SelectedValueProperty).UpdateSource();
+            this.SelectProductComboBox.GetBindingExpression(ComboBox.SelectedValueProperty).UpdateSource();
+        }
+
+        private void InsertButton_Click(object sender, RoutedEventArgs e)
+        {
+            #region validation 
+            ForceValidation();
+            if (Validation.GetHasError(SerialNoTextBox) ||
+                Validation.GetHasError(StockLevelTextBox) ||
+                Validation.GetHasError(NameTextBox) ||
+                Validation.GetHasError(MakeComboBox) ||
+                Validation.GetHasError(LocationComboBox) ||
+                Validation.GetHasError(SelectProductComboBox)
+                )
+            {
+                var sMessageDialog = new MessageDialog
+                {
+                    Message = { Text =
+                    "ERROR: Fill required fields!" }
+                };
+
+                DialogHost.Show(sMessageDialog, "RootDialog");
+                return;
+            }
+
+            if (SelectProductComboBox.SelectedItem == null)
+            {
+                var sMessageDialog = new MessageDialog
+                {
+                    Message = { Text =
+                    "ERROR: Select a Product!" }
+                };
+
+                DialogHost.Show(sMessageDialog, "RootDialog");
+                return;
+            }
+
+            if (MakeComboBox.SelectedItem == null)
+            {
+                var sMessageDialog = new MessageDialog
+                {
+                    Message = { Text =
+                    "ERROR: Select a Make Name!" }
+                };
+
+                DialogHost.Show(sMessageDialog, "RootDialog");
+                return;
+            }
+
+            if (LocationComboBox.SelectedItem == null)
+            {
+                var sMessageDialog = new MessageDialog
+                {
+                    Message = { Text =
+                    "ERROR: Select a Location!" }
+                };
+
+                DialogHost.Show(sMessageDialog, "RootDialog");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(StockLevelTextBox.Text) || !(int.TryParse(StockLevelTextBox.Text, out int a)))
+            {
+                var sMessageDialog = new MessageDialog
+                {
+                    Message = { Text =
+                    "ERROR: Enter valid Stock level\n(Numerical value)!" }
+                };
+
+                DialogHost.Show(sMessageDialog, "RootDialog");
+                return;
+            }
+
+            if (int.Parse(StockLevelTextBox.Text) < 0)
+            {
+                var sMessageDialog = new MessageDialog
+                {
+                    Message = { Text =
+                    "ERROR: Enter right value for stock.." }
+                };
+
+                DialogHost.Show(sMessageDialog, "RootDialog");
+                return;
+            }
+            #endregion
+
+            bool flag1 = false;
+            bool flag2 = false;
+
+            if (new ProductFactory().Update(int.Parse(SelectProductComboBox.SelectedValue.ToString()),
+                SerialNoTextBox.Text,
+                NameTextBox.Text,
+                int.Parse(MakeComboBox.SelectedValue.ToString())))
+            {
+                flag1 = true;
+            }
+            else
+                flag1 = false;
+
+            if (new InventoryFactory().Update(int.Parse(SelectProductComboBox.SelectedValue.ToString()),
+                int.Parse(StockLevelTextBox.Text.ToString()),
+               int.Parse(LocationComboBox.SelectedValue.ToString())))
+            {
+                flag2 = true;
+            }
+            else
+                flag2 = false;
+
+            if (flag1 || flag2)
+            {
+                var sMessageDialog = new MessageDialog
+                {
+                    Message = { Text = "Updated Info!" }
+                };
+
+                DialogHost.Show(sMessageDialog, "RootDialog");
+            }
+            else
+            {
+                var sMessageDialog = new MessageDialog
+                {
+                    Message = { Text = "Unable to update Info" }
+                };
+
+                DialogHost.Show(sMessageDialog, "RootDialog");
+            }
+        }
+
+        private void SelectProductComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateFields();
+        }
+
+        void UpdateFields()
+        {
+            if (SelectProductComboBox.SelectedValue != null)
+            {
+                var id = int.Parse(SelectProductComboBox.SelectedValue.ToString());
+                var prod = new ProductFactory().SelectAll().Where(x => x.ID == id).FirstOrDefault();
+                if (prod != null)
+                {
+                    SerialNoTextBox.Text = prod.SerialNo;
+                    NameTextBox.Text = prod.Name;
+                    MakeComboBox.SelectedValue = prod.MakeID;
+                }
+
+                var inven = new InventoryFactory().SelectAll().Where(x => x.ID == id).FirstOrDefault();
+                if (inven != null)
+                {
+                    LocationComboBox.SelectedValue = inven.LocationID;
+                    StockLevelTextBox.Text = inven.StockLevel.ToString();
+                }
+            }
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            Clear();
+        }
+
+        private void Clear()
+        {
+            this.SelectProductComboBox.SelectedItem = null;
+            this.MakeComboBox.SelectedItem = null;
+            this.SerialNoTextBox.Clear();
+            this.NameTextBox.Clear();
+            this.StockLevelTextBox.Clear();
         }
     }
 }
