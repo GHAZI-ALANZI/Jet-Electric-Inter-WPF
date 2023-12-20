f0b66281876b82f7f1d2f7eee2c1bdc6ad91b67e
GHAZI ALANZI <105205339+GHAZI-ALANZI@users.noreply.github.com>
2023-12-20 16:17:01 +0100

    Supplier views Add,Edit and List succes created

--- Jet-Electric Inter/Views/SupplierList.xaml.cs
+++ Jet-Electric Inter/Views/SupplierList.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Jet_Electric_DataAccessLayer.AppDbContext;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Jet_Electric_Inter.Views
 {
@@ -23,6 +12,56 @@ namespace Jet_Electric_Inter.Views
         public SupplierList()
         {
             InitializeComponent();
+            LoadData();
+            PopupBox.Visibility = Visibility.Visible;
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        void LoadData()
+        {
+            DataGrid.ItemsSource = new JetDbContext().Contacts.ToList();
+        }
+
+        private void PopUp_AddNewSupplier(object sender, RoutedEventArgs e)
+        {
+
+
+            TextBlock_TitleName.Visibility = Visibility.Collapsed;
+            DataGrid.Visibility = Visibility.Collapsed;
+            PopupBox.Visibility = Visibility.Collapsed;
+
+            SupplierAddNew x = new SupplierAddNew();
+            UserPages.Children.Clear();
+            UserPages.Children.Add(x);
+            PopupBoxWithClose.Visibility = Visibility.Visible;
+        }
+
+        private void PopUp_Close(object sender, RoutedEventArgs e)
+        {
+            UserPages.Children.Clear();
+            PopupBoxWithClose.Visibility = Visibility.Hidden;
+
+            LoadData();
+            TextBlock_TitleName.Visibility = Visibility.Visible;
+            DataGrid.Visibility = Visibility.Visible;
+            PopupBox.Visibility = Visibility.Visible;
+        }
+
+        private void PopUp_EditSupplier(object sender, RoutedEventArgs e)
+        {
+            TextBlock_TitleName.Visibility = Visibility.Collapsed;
+            DataGrid.Visibility = Visibility.Collapsed;
+            PopupBox.Visibility = Visibility.Collapsed;
+
+            SupplierEdit x = new SupplierEdit();
+            UserPages.Children.Clear();
+            UserPages.Children.Add(x);
+            PopupBoxWithClose.Visibility = Visibility.Visible;
+        }
+
     }
 }
